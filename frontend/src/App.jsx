c3539326f837adc93562ea39494c21d0753ed3dd
Nihal Shahid <n1704692@gmail.com>
2026-08-15 17:24:10 +0500

    initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <div className="card">
+        <h1>Hello World! 👋</h1>
+        <h2>I'm a Frontend Developer 💻</h2>
+        <p>
+          Welcome to my React application.
+          <br />
+          I love building beautiful and responsive websites.
+        </p>
+
+        <button>Get Started 🚀</button>
+      </div>
+    </div>
+  )
+}
+
+export default App
